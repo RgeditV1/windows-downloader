4822f0a1127c329419ce320b1088e91ffc4172e3
Rgeditv1 <rgeditv1@protonmail.com>
2026-09-07 15:53:05 -0400

    lectura de datos creada

--- include/downloader.hpp
+++ include/downloader.hpp
@@ -1,6 +1,38 @@
 #pragma once
+#include <cstdlib>
+#include <cstdlib>
 #include <string>
+#include <vector>
 
-//void download_file(const std::string& url, const std::string& output_path);
+#include <curl/curl.h>
+#include <nlohmann/json.hpp>
 
-void hello_world();
+using json = nlohmann::json;
+
+CURL* curl;
+CURLcode result;
+std::string response; // Datos recibidos por Curl
+
+enum class Architecture {
+    x86,
+    x64,
+    ARM64,
+    Multi,
+    Unknown
+};
+
+const std::string RAW = "https://raw.githubusercontent.com/RgeditV1/windows-downloader/main/iso.json";
+struct IsoInfo {
+    std::string title;
+    std::string url;
+    std::string language;
+    std::string architecture;
+    uint32_t build;
+    uint64_t size;
+};
+
+std::vector<IsoInfo> parseIsoInfo(const std::string& response);
+
+// Callback que recibe los datos descargados
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* output);
+void InitCurl();
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,84 @@
 #include <iostream>
 #include "downloader.hpp"
 
-void hello_world() {
-    std::cout << "Hello, World!" << std::endl;
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* output)
+{
+    size_t totalSize = size * nmemb;
+
+    output->append(
+        static_cast<char*>(contents),
+        totalSize
+    );
+
+    return totalSize;
+}
+
+void InitCurl(){
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, RAW.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+        result = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+    }
+    curl_global_cleanup();
+}
+
+std::vector<IsoInfo> parseIsoInfo(const std::string& response)
+{
+    std::vector<IsoInfo> isos;
+
+    json data = json::parse(response);
+
+    for (const auto& item : data)
+    {
+        IsoInfo iso;
+
+        iso.title = item.value("title", "");
+        iso.url = item.value("link", "");
+        iso.language = item.value("language", "");
+        iso.architecture = item.value("architecture", "");
+        iso.build = item.value("build", 0);
+
+        // Por ahora
+        iso.size = 0;
+
+        isos.push_back(iso);
+    }
+
+    return isos;
 }
 
 int main() {
-    //download_file("https://example.com/file.zip", "output.zip");
-    hello_world();
+
+    InitCurl();
+
+    if (result == CURLE_OK)
+    {
+        try
+        {
+            std::vector<IsoInfo> isos = parseIsoInfo(response);
+
+            for (const auto& iso : isos)
+            {
+                std::cout << "Title: " << iso.title << '\n';
+                std::cout << "URL: " << iso.url << '\n';
+                std::cout << "Language: " << iso.language << '\n';
+                std::cout << "Architecture: " << iso.architecture << '\n';
+                std::cout << "Build: " << iso.build << '\n';
+                std::cout << "Size: " << iso.size << '\n';
+
+                std::cout << "------------------\n";
+            }
+        }
+        catch (const json::exception& e)
+        {
+            std::cerr << "JSON error: " << e.what() << '\n';
+        }
+    }
+
     return 0;
 }
